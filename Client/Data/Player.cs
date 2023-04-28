28ec90433cab96769bd3e144808aef60a286269c
riguy506 <riguy506@gmail.com>
2023-04-28 04:17:00 -0400

    yuh

---
+++ Client/Data/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShootsNLadders.Client.Data
+{
+
+	public class Player
+	{
+		public int pos { get; set; }
+		public int id { get; set; }
+		public int win_total { get; set; }
+		public string clr { get; set; }
+	}
+
+}
+
